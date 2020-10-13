73c993a1eb4b75a02fa2ba053938bc6ead5663b8
Florian Bossert <fbossert85@gmail.com>
2020-10-13 22:55:59 +0200

    PortableUpdater-Class erweitert
    CheckForUpdateAsync() hinzugefügt

---
+++ PortableUpdater.NET/PortableUpdater.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace PortableUpdaterDotNET
+{
+    public class PortableUpdater
+    {        
+        public Uri XmlLink { get; private set; }
+
+        public void Start(string xmlLink)
+        {
+            try
+            {
+                XmlLink = new Uri(xmlLink);
+                if(XmlLink.Scheme.Equals(Uri.UriSchemeHttp) || XmlLink.Scheme.Equals(Uri.UriSchemeHttps))
+                {
+                    CheckForUpdateAsync();
+                }
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+            catch(UriFormatException)
+            {
+
+            }
+        }
+
+        private async Task CheckForUpdateAsync()
+        {
+            HttpClient client = new HttpClient();
+            using (HttpResponseMessage response = await client.GetAsync(XmlLink))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string result = await content.ReadAsStringAsync();
+                    Console.WriteLine($"*** {result} ***");
+                }
+            }
+        }
+    }
+}
---
+++ PortableUpdaterTest.WPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PortableUpdaterDotNET;
+using System.Windows;
+
+
+namespace PortableUpdaterTest.WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Test();
+            
+        }
+
+        private async void Test()
+        {
+            var updatemanager = new PortableUpdater();
+            updatemanager.Start(@"https://raw.githubusercontent.com/flobo85/PortableUpdater.NET/main/Test.txt");
+        }
+    }
+}
